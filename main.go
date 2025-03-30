4e62079f4ab0f26a0b920c11126229ff44d74e5d
Omnis <87337111+VVandereer@users.noreply.github.com>
2025-03-31 00:23:19 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"femtolab/devices"
+)
+
+func main() {
+	DelayLine,err := devices.InitStepperMotor("/dev/ttyUSB0", 115200)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Включаем моторчик
+	if err := DelayLine.Enable(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Играемся с параметрами
+	if err := DelayLine.SetAcceleration(6400); err != nil {
+		log.Fatal(err)
+	}
+	if err := DelayLine.SetMaxSpeed(3200); err != nil {
+		log.Fatal(err)
+	}
+	// Перемещаем мотор
+	if err := DelayLine.Move(1000); err != nil {
+		log.Fatal(err)
+	}
+
+	// Запрашиваем текущую позицию моторчика
+	position, err := DelayLine.AskPosition()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Текущая позиция мотора: %d\n", position)
+
+	// Отключаем мотор
+	if err := DelayLine.Disable(); err != nil {
+		log.Fatal(err)
+	}
+}
